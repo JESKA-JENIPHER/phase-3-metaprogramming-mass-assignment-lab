a013a0d5778fcacc1346bce2a6e86de907ffa93e
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-08-21 12:04:02 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
   # your code here
+  def initialize(attributes)
+    #to iterate through the attributes
+    attributes.each do |key, value|
+      # create a getter and setter by calling the attr_accessor method
+      self.class.attr_accessor(key)
+      #calls a method on an object
+      self.send("#{key}=", value)
+    end
+  end
 end
